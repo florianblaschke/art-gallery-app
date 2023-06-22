a8f3ec58bf042315792bdf44f9596c8a2d69ac36
Florian Blaschke <florianblaschke@web.de>
2023-06-22 17:51:50 +0200

    refactor success

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import Spotlight from "@/components/Spotlight";
 import useSWR from "swr";
-export default function HomePage() {
-  const url = "https://example-apis.vercel.app/api/art";
-  const { data, error } = useSWR(url);
-  if (error) return <div>Something bad happened</div>;
-  if (!data) return <div>...Loading</div>;
-  const rdmN = Math.floor(Math.random() * data.length);
+export default function HomePage({ picture }) {
+  const rdmN = Math.floor(Math.random() * picture.length);
   return (
     <div>
-      <Spotlight picture={data[rdmN]} />
+      <Spotlight picture={picture[rdmN]} />
     </div>
   );
 }
